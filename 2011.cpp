db5034e0436778b31973a2787dbc77a35ebc5c60
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-05-27 17:55:27 +0530

    Final Value of Variable After Performing Operations

---
+++ 2011.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(int i=0; i<(operations.size()); i++){
+            if(operations[i][0] == '-'){
+                x--;
+            }
+            else if(operations[i][0] == '+'){
+                x++;
+            }
+            else{
+                if(operations[i][1] == '+'){
+                    x++;
+                }
+                else{
+                    x--;
+                }
+            }
+        }
+        return x;
+    }
+};
