50ee04d85644f7b32da52d9fffb47de116d81254
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-05-31 17:55:53 +0530

    Insert Interval

---
+++ 57.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+    
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i=1; i<intervals.size(); i++){
+            if(intervals[i][0] <= ans[ans.size()-1][1]){
+                if(intervals[i][1] <= ans[ans.size()-1][1]){
+                    continue;
+                }
+                else{
+                    ans[ans.size()-1][1] = intervals[i][1];
+                }
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
