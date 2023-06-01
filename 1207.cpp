c3a6f76f0c47ed5203d6322ecdaa24a4b3867ab2
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-06-02 00:25:42 +0530

    https://leetcode.com/problems/unique-number-of-occurrences/description/?envType=list&envId=r97thsrm

---
+++ 1207.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> first_map;
+        for(int i=0; i<arr.size(); i++){
+            first_map[arr[i]] += 1;
+        }
+        map<int,int> second_map;
+
+        for(auto &x:first_map){
+            if(second_map[x.second] != 0){
+                return false;
+            }
+            second_map[x.second] += 1;
+        }
+        return true;
+    }
+};
