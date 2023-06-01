0f754f741054733ed34c723dfdc63858e7c28da1
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-06-02 00:26:12 +0530

    Unique Number of Occurrences

--- 1207.cpp
+++ 1207.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) {
+    bool uniqueOccurrences(vector<int> & arr) {
         map<int,int> first_map;
         for(int i=0; i<arr.size(); i++){
             first_map[arr[i]] += 1;
